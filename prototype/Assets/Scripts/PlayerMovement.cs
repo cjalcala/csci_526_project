25a71f66c4ff685616a6db09b4a595eb8b8a46e1
Devang Sharma <sharmadevang11@gmail.com>
2022-09-11 12:18:34 -0700

    Adding Distance Remaining Metric

--- prototype/Assets/Scripts/PlayerMovement.cs
+++ prototype/Assets/Scripts/PlayerMovement.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -10,7 +11,7 @@ public class PlayerMovement : MonoBehaviour
     public float horizontalMultiplier = 2;
     public float jumpForce = 600f;
     public LayerMask groundMask;
-
+    public Text distanceRemaining;
     float horizontalInput;
 
     private void FixedUpdate()
@@ -35,6 +36,8 @@ public class PlayerMovement : MonoBehaviour
         if(transform.position.y < -5) {
             Die();
         }
+
+        distanceRemaining.text = "Distance Remaining: " + (150-rb.position.z).ToString("0"); 
     }
 
     public void Die()
@@ -56,5 +59,10 @@ public class PlayerMovement : MonoBehaviour
 
         // If we are, jump
         rb.AddForce(Vector3.up*jumpForce);
+    }
+
+    void Start()
+    {
+        distanceRemaining = GameObject.Find("DistanceText").GetComponent<Text>();
     }
 }
