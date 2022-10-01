29df50b80ca09f6657f795badfefa07cb94827b1
Devang Sharma <sharmadevang11@gmail.com>
2022-10-01 11:39:06 -0700

    Add Time Label In Tutorial Scene

--- prototype/Assets/Scripts/TutorialGameManager.cs
+++ prototype/Assets/Scripts/TutorialGameManager.cs
@@ -10,6 +10,9 @@ public class TutorialGameManager : MonoBehaviour
     public static TutorialGameManager inst;
     public Text tutorialCoinText;
     public static int tutCoinCnt;
+    public Text timeText; 
+    public float time = 120;
+    TutorialPlayerMovement tutorialplayerMovement;
     // Start is called before the first frame update
 
     private void Awake()
@@ -20,17 +23,27 @@ public class TutorialGameManager : MonoBehaviour
     public void IncrementScore()
     {
         score++;
-        tutorialCoinText.text= "        : " + score;
+        tutorialCoinText.text="Coin: "+score;
     }
 
     void Start()
     {
-        
+        tutorialplayerMovement = GameObject.FindObjectOfType<TutorialPlayerMovement>();
     }
 
     // Update is called once per frame
     void Update()
     {
         tutCoinCnt=score;
+        if (time > 0)
+        {
+            time -= Time.deltaTime;
+            timeText.text = ": " + time.ToString("0") + " Sec";
+
+        }
+        else
+        {
+            tutorialplayerMovement.Die();
+        }
     }
 }
