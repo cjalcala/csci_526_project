acb702c20fcee0487e759ffc54f57da08f1e6337
Gaurav Makasare <makasare@usc.edu>
2022-10-11 14:26:40 -0700

    Play pause music on button click fixed

--- prototype/Assets/Scripts/PauseMenu.cs
+++ prototype/Assets/Scripts/PauseMenu.cs
@@ -9,17 +9,22 @@ public class PauseMenu : MonoBehaviour
     // Start is called before the first frame update
    [SerializeField] public GameObject PauseMenuPanel;
    public static bool GameIsPaused = false;
+   public GameObject Obj;
 
    public void Pause() {
     PauseMenuPanel.SetActive(true);
     Time.timeScale = 0f;
     GameIsPaused = true;
+    Obj = GameObject.FindGameObjectWithTag("GameMusic");
+    Obj.SetActive(false);
    }
 
    public void Resume() {
     PauseMenuPanel.SetActive(false);
     Time.timeScale = 1f;
     GameIsPaused = false;
+    //Obj = GameObject.FindGameObjectsWithTag('GameMusic');
+    Obj.SetActive(true);
    }
 
    public void Restart() {
@@ -33,6 +38,7 @@ public class PauseMenu : MonoBehaviour
     ScoreTracker.ingredientsList.Add("Steak", new Ingredient("Steak", 1));
     Welcome.immunity = false;
     SceneManager.LoadScene("Game");
+    Obj.SetActive(true);
    }
 
 
@@ -40,6 +46,7 @@ public class PauseMenu : MonoBehaviour
     Time.timeScale = 1f;
     GameIsPaused = true;
     SceneManager.LoadScene("WelcomeScreen");
+    Obj.SetActive(true);
    }
 
 }
