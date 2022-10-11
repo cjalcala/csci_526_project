941c5e7c37b1663ae3312b9eec5a23a997e86afc
tommyshang <56244127+tommyshang@users.noreply.github.com>
2022-10-11 01:47:12 -0700

    Update SanctumQuiz.cs
    Format

--- prototype/Assets/Scripts/SanctumQuiz.cs
+++ prototype/Assets/Scripts/SanctumQuiz.cs
@@ -6,8 +6,7 @@ using UnityEngine.SceneManagement;
 using UnityEngine.Networking;
 
 
-public class SanctumQuiz : MonoBehaviour
-{
+public class SanctumQuiz : MonoBehaviour {
     [SerializeField] private string URL;
     public List<QuizQA> questionAnswers;
     public GameObject[] options;
@@ -25,18 +24,15 @@ public class SanctumQuiz : MonoBehaviour
     public Text sanctumCoins;
 
     public QuizQA quizQuestion;
-    private void Start()
-    {
+    private void Start() {
         //coin = GameObject.Find("CoinText").GetComponent<Text>();
         //numCoins = tempCoinvalue;
         //coin.text = "Coins : " + numCoins.ToString();
-        if (TutorialManager.tutorialActive)
-        {
+        if (TutorialManager.tutorialActive) {
             TutorialGameManager.tutCoinCnt -= 2;
             sanctumCoins.text = "Coins : " + TutorialGameManager.tutCoinCnt.ToString();
         }
-        else
-        {
+        else {
             sanctumCoins.text = "Coins : " + ScoreTracker.coins.ToString();
         }
 
@@ -46,89 +42,65 @@ public class SanctumQuiz : MonoBehaviour
         questionGenerator(0.7, 0.3, 0);
     }
 
-    public void retry()
-    {
+    public void retry() {
         ScoreTracker.coins -= 10;
         //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         QuizPanel.SetActive(true);
         BPanel.SetActive(false);
     }
 
-    void gameOver()
-    {
+    void gameOver() {
         QuizPanel.SetActive(false);
         BPanel.SetActive(true);
 
         coin.text = "Coins : " + ScoreTracker.coins.ToString();
     }
 
-    public void correct()
-    {
+    public void correct() {
         //Send(currQuestion, 1, 0);
-        //gameOver();
-
-        // if(ScoreTracker.uncompletedIngredients().Count > 0)
-        // {
-        //     List<string> keyList = ScoreTracker.uncompletedIngredients();
-        //     int randValue = Random.Range(0, keyList.Count);
-        //     ScoreTracker.increaseIngredient(keyList[randValue]);
-        // }
-        if (TutorialManager.tutorialActive)
-        {
+        if (TutorialManager.tutorialActive) {
             Invoke("LoadTutorialComplete", 1.5f);
         }
-        else
-        {
+        else {
             string Ingredient = PlayerPrefs.GetString("IngredientID");// Change to index later
             ScoreTracker.increaseIngredient(Ingredient);//use map to find the ingredient string /change increaseIngredient param to index
             SceneManager.LoadScene("Game");
         }
     }
 
-    public void LoadTutorialComplete()
-    {
+    public void LoadTutorialComplete() {
         SceneManager.LoadScene("TutorialComplete");
     }
 
-    public void wrong()
-    {
+    public void wrong() {
 
-        if (TutorialManager.tutorialActive)
-        {
-            if (TutorialGameManager.tutCoinCnt < 10)
-            {
+        if (TutorialManager.tutorialActive) {
+            if (TutorialGameManager.tutCoinCnt < 10) {
                 Invoke("restartTutorial", 2.0f);
             }
-            else
-            {
+            else {
                 Invoke("reloadSanctum", 1.5f);
 
             }
         }
-        else
-        {
+        else {
 
             //Send(currQuestion, 0, 1);
 
-            if (ScoreTracker.coins < 10)
-            {
+            if (ScoreTracker.coins < 10) {
                 continueGame();
             }
-            else
-            {
+            else {
                 Invoke("gameOver", 2.0f);
             }
-
         }
     }
 
-    void restartTutorial()
-    {
+    void restartTutorial() {
         SceneManager.LoadScene("TutorialGame");
     }
 
-    void reloadSanctum()
-    {
+    void reloadSanctum() {
         TutorialGameManager.tutCoinCnt -= 8;
         //SceneManager.LoadScene("Sanctum");
         QuizPanel.SetActive(false);
@@ -138,75 +110,58 @@ public class SanctumQuiz : MonoBehaviour
     }
 
 
-    public void continueGame()
-    {
+    public void continueGame() {
         SceneManager.LoadScene("Game");
     }
 
-    void setQnA()
-    {
+    void setQnA() {
         questionText.text = quizQuestion.question;
-        for (int i = 0; i < options.Length; i++)
-        {
+        for (int i = 0; i < options.Length; i++) {
             options[i].GetComponent<AnswerScript>().isCorrect = false;
             options[i].transform.GetChild(0).GetComponent<Text>().text = quizQuestion.answers[i];
 
-            if (quizQuestion.correctAnswer == i)
-            {
+            if (quizQuestion.correctAnswer == i) {
                 options[i].GetComponent<AnswerScript>().isCorrect = true;
             }
         }
     }
 
-    void questionGenerator(double easyRate, double mediumRate, double hardRate)
-    {
-        if (TutorialManager.tutorialActive)
-        {
+    void questionGenerator(double easyRate, double mediumRate, double hardRate) {
+        if (TutorialManager.tutorialActive) {
             quizQuestion = TutorialManager.questionGenerator.getQuestion(easyRate, mediumRate, hardRate);
         }
-        else
-        {
+        else {
             quizQuestion = GameManager.questionGenerator.getQuestion(easyRate, mediumRate, hardRate);
         }
 
-        if (quizQuestion != null)
-        {
+        if (quizQuestion != null) {
             setQnA();
         }
-        else
-        {
+        else {
             Debug.Log("Out of questions");
             gameOver();
         }
     }
 
 
-    public void Send(int question_id, int c, int w)
-    {
+    public void Send(int question_id, int c, int w) {
         StartCoroutine(Post(question_id.ToString(), c.ToString(), w.ToString()));
 
     }
 
-    private IEnumerator Post(string question_id, string c, string w)
-    {
+    private IEnumerator Post(string question_id, string c, string w) {
 
         WWWForm form = new WWWForm();
         form.AddField("entry.1035881353", question_id);
         form.AddField("entry.1651523432", c);
         form.AddField("entry.281241967", w);
 
-
-
-
-        using (UnityWebRequest www = UnityWebRequest.Post(URL, form))
-        {
+        using (UnityWebRequest www = UnityWebRequest.Post(URL, form)) {
             yield return www.SendWebRequest();
-            if (www.result != UnityWebRequest.Result.Success)
-            {
+            if (www.result != UnityWebRequest.Result.Success) {
                 Debug.Log(www.error);
             }
-            else
-            {
+            else {
                 Debug.Log("Form upload complete!");
             }
 
@@ -214,5 +169,4 @@ public class SanctumQuiz : MonoBehaviour
         }
 
     }
-
 }
