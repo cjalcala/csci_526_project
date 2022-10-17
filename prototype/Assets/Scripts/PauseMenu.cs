ea0242efe2dcba3822323e680f6e9ab00f084b57
Gaurav Makasare <makasare@usc.edu>
2022-10-17 15:52:25 -0700

    Fixed Sound switch

--- prototype/Assets/Scripts/PauseMenu.cs
+++ prototype/Assets/Scripts/PauseMenu.cs
@@ -10,13 +10,18 @@ public class PauseMenu : MonoBehaviour
    [SerializeField] public GameObject PauseMenuPanel;
    public static bool GameIsPaused = false;
    public GameObject Obj;
+   public static int audio_flag = 0;
 
    public void Pause() {
     PauseMenuPanel.SetActive(true);
     Time.timeScale = 0f;
     GameIsPaused = true;
     Obj = GameObject.FindGameObjectWithTag("GameMusic");
-    Obj.SetActive(false);
+    //Obj.SetActive(false);
+    if (AudioListener.pause == false){ //music was playing
+        audio_flag = 1;
+    }
+    AudioListener.pause = true;
    }
 
    public void Resume() {
@@ -24,7 +29,12 @@ public class PauseMenu : MonoBehaviour
     Time.timeScale = 1f;
     GameIsPaused = false;
     //Obj = GameObject.FindGameObjectsWithTag('GameMusic');
-    Obj.SetActive(true);
+    //Obj.SetActive(true);
+    if (audio_flag == 1){
+        AudioListener.pause = false;
+    }else{
+        AudioListener.pause = true;
+    }
    }
 
    public void Restart() {
@@ -38,7 +48,12 @@ public class PauseMenu : MonoBehaviour
     ScoreTracker.ingredientsList.Add("Steak", new Ingredient("Steak", 1, 2));
     Welcome.immunity = false;
     SceneManager.LoadScene("Game");
-    Obj.SetActive(true);
+    //Obj.SetActive(true);
+    if (audio_flag == 1){
+        AudioListener.pause = false;
+    }else{
+        AudioListener.pause = true;
+    }
    }
 
 
@@ -46,7 +61,11 @@ public class PauseMenu : MonoBehaviour
     Time.timeScale = 1f;
     GameIsPaused = true;
     SceneManager.LoadScene("WelcomeScreen");
-    Obj.SetActive(true);
+    if (audio_flag == 1){
+        AudioListener.pause = false;
+    }else{
+        AudioListener.pause = true;
+    }
    }
 
 }
