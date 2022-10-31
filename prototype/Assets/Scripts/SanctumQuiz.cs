79b8de69a039dd47df8a7d987380d415c3395698
Jiangyun Wang <jurainyun@gmail.com>
2022-10-31 02:11:50 -0700

    show correct answer when no enough coins

--- prototype/Assets/Scripts/SanctumQuiz.cs
+++ prototype/Assets/Scripts/SanctumQuiz.cs
@@ -6,7 +6,8 @@ using UnityEngine.SceneManagement;
 using UnityEngine.Networking;
 using System;
 
-public class SanctumQuiz : MonoBehaviour {
+public class SanctumQuiz : MonoBehaviour
+{
     [SerializeField] private string URL;
     [SerializeField] private AudioSource sanctum_entry_sound;
     public List<QuizQA> questionAnswers;
@@ -29,105 +30,122 @@ public class SanctumQuiz : MonoBehaviour {
 
     public QuizQA quizQuestion;
     string quizIngredient;// Change to index later
-    private void Start() {
+    private void Start()
+    {
         //coin = GameObject.Find("CoinText").GetComponent<Text>();
         //numCoins = tempCoinvalue;
         //coin.text = "Coins : " + numCoins.ToString();
         sanctum_entry_sound.Play();
 
-        if(TutorialManager.tutorialActive)
+        if (TutorialManager.tutorialActive)
         {
-            TutorialGameManager.tutCoinCnt-=2;
+            TutorialGameManager.tutCoinCnt -= 2;
             sanctumCoins.text = "Coins : " + TutorialGameManager.tutCoinCnt.ToString();
         }
-        else {
+        else
+        {
             sanctumCoins.text = "Coins : " + GameTracker.coins.ToString();
         }
 
-        Debug.Log("Sanctum "+GameTracker.timeRemain);
+        Debug.Log("Sanctum " + GameTracker.timeRemain);
         //Debug.Log(TutorialManager.tutorialActive);
         BPanel.SetActive(false);
         quizIngredient = PlayerPrefs.GetString("IngredientID");// Change to index later
         questionGenerator(0.7, 0.3, 0);
     }
 
-    public void retry() {
+    public void retry()
+    {
         GameTracker.coins -= 10;
         //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         QuizPanel.SetActive(true);
         BPanel.SetActive(false);
         Button[] buttons = QuizPanel.GetComponentsInChildren<Button>();
-        for(int b=0; b<4; b++)
+        for (int b = 0; b < 4; b++)
         {
             buttons[b].enabled = true;
         }
         sanctumCoins.text = "Coins : " + GameTracker.coins.ToString();
     }
 
-    void gameOver() {
+    void gameOver()
+    {
         QuizPanel.SetActive(false);
         BPanel.SetActive(true);
 
         coin.text = "Coins : " + GameTracker.coins.ToString();
     }
 
-    public void correct() {
+    public void correct()
+    {
 
         Button[] buttons = QuizPanel.GetComponentsInChildren<Button>();
-        for(int b=0; b<4; b++)
+        for (int b = 0; b < 4; b++)
         {
             buttons[b].enabled = false;
         }
-        
-       Send(quizQuestion.question, 1, 0);
-        if (TutorialManager.tutorialActive) {
+
+        Send(quizQuestion.question, 1, 0);
+        if (TutorialManager.tutorialActive)
+        {
             Invoke("LoadTutorialComplete", 1.5f);
         }
-        else {
+        else
+        {
             GameTracker.increaseIngredient(quizIngredient);//use map to find the ingredient string /change increaseIngredient param to index
             GameTracker.LoadScenes();
         }
     }
 
-    public void LoadTutorialComplete() {
+    public void LoadTutorialComplete()
+    {
         SceneManager.LoadScene("TutorialComplete");
     }
 
-    public void wrong() {
+    public void wrong()
+    {
 
         Button[] buttons = QuizPanel.GetComponentsInChildren<Button>();
-        for(int b=0; b<4; b++)
+        for (int b = 0; b < 4; b++)
         {
             buttons[b].enabled = false;
         }
 
-        if (TutorialManager.tutorialActive) {
-            if (TutorialGameManager.tutCoinCnt < 10) {
+        if (TutorialManager.tutorialActive)
+        {
+            if (TutorialGameManager.tutCoinCnt < 10)
+            {
                 Invoke("restartTutorial", 2.0f);
             }
-            else {
+            else
+            {
                 Invoke("reloadSanctum", 1.5f);
 
             }
         }
-        else {
+        else
+        {
 
             Send(quizQuestion.question, 0, 1);
 
-            if (GameTracker.coins < 10) {
+            if (GameTracker.coins < 10)
+            {
                 continueGame();
             }
-            else {
+            else
+            {
                 Invoke("gameOver", 2.0f);
             }
         }
     }
 
-    void restartTutorial() {
+    void restartTutorial()
+    {
         SceneManager.LoadScene("TutorialGame");
     }
 
-    void reloadSanctum() {
+    void reloadSanctum()
+    {
         TutorialGameManager.tutCoinCnt -= 8;
         //SceneManager.LoadScene("Sanctum");
         QuizPanel.SetActive(false);
@@ -137,60 +155,75 @@ public class SanctumQuiz : MonoBehaviour {
     }
 
 
-    public void continueGame() {
+    public void continueGame()
+    {
         GameTracker.LoadScenes();
     }
 
-    void setQnA() {
+    void setQnA()
+    {
         questionText.text = quizQuestion.question;
-        for (int i = 0; i < options.Length; i++) {
+        for (int i = 0; i < options.Length; i++)
+        {
             options[i].GetComponent<AnswerScript>().isCorrect = false;
             options[i].transform.GetChild(0).GetComponent<Text>().text = quizQuestion.answers[i];
+            options[i].GetComponent<AnswerScript>().correctIdx = quizQuestion.correctAnswer;
 
-            if (quizQuestion.correctAnswer == i) {
+            if (quizQuestion.correctAnswer == i)
+            {
                 options[i].GetComponent<AnswerScript>().isCorrect = true;
             }
         }
     }
 
-    void questionGenerator(double easyRate, double mediumRate, double hardRate) {
-        if (TutorialManager.tutorialActive) {
+    void questionGenerator(double easyRate, double mediumRate, double hardRate)
+    {
+        if (TutorialManager.tutorialActive)
+        {
             quizQuestion = TutorialManager.questionGenerator.getQuestion(easyRate, mediumRate, hardRate);
             var qT = TutorialManager.questionGenerator.getIngredientQuestion(quizIngredient);
         }
-        else {
+        else
+        {
             quizQuestion = GameTracker.questionGenerator.getQuestion(easyRate, mediumRate, hardRate);
             var qT = GameTracker.questionGenerator.getIngredientQuestion(quizIngredient);
         }
 
-        if (quizQuestion != null) {
+        if (quizQuestion != null)
+        {
             setQnA();
         }
-        else {
+        else
+        {
             Debug.Log("Out of questions");
             gameOver();
         }
     }
 
 
-    public void Send(string question_id, int c, int w) {
+    public void Send(string question_id, int c, int w)
+    {
         StartCoroutine(Post(question_id, c.ToString(), w.ToString()));
 
     }
 
-    private IEnumerator Post(string question_id, string c, string w) {
+    private IEnumerator Post(string question_id, string c, string w)
+    {
 
         WWWForm form = new WWWForm();
         form.AddField("entry.1035881353", question_id);
         form.AddField("entry.1651523432", c);
         form.AddField("entry.281241967", w);
 
-        using (UnityWebRequest www = UnityWebRequest.Post(URL, form)) {
+        using (UnityWebRequest www = UnityWebRequest.Post(URL, form))
+        {
             yield return www.SendWebRequest();
-            if (www.result != UnityWebRequest.Result.Success) {
+            if (www.result != UnityWebRequest.Result.Success)
+            {
                 Debug.Log(www.error);
             }
-            else {
+            else
+            {
                 Debug.Log("Form upload complete!");
             }
 
@@ -201,12 +234,12 @@ public class SanctumQuiz : MonoBehaviour {
 
     void Update()
     {
-        if(!TutorialManager.tutorialActive)
+        if (!TutorialManager.tutorialActive)
         {
             if (GameTracker.timeRemain > 0)
             {
                 GameTracker.timeRemain -= Time.deltaTime;
-                TimeSlider.fillAmount = GameTracker.timeRemain/GameTracker.originalTime;
+                TimeSlider.fillAmount = GameTracker.timeRemain / GameTracker.originalTime;
                 timeText.text = ": " + GameTracker.timeRemain.ToString("0") + " Sec";
 
                 int forwardSeconds = (int)GameTracker.originalTime - Convert.ToInt32(Math.Truncate(GameTracker.timeRemain));
@@ -228,7 +261,7 @@ public class SanctumQuiz : MonoBehaviour {
             if (TutorialGameManager.time > 0)
             {
                 TutorialGameManager.time -= Time.deltaTime;
-                TimeSlider.fillAmount = TutorialGameManager.time/90;
+                TimeSlider.fillAmount = TutorialGameManager.time / 90;
                 timeText.text = ": " + TutorialGameManager.time.ToString("0") + " Sec";
             }
             else
