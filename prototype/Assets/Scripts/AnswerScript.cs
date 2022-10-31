b299a4446c89bac49d39c25b6cfeb7dbd74ed3f6
Jiangyun Wang <jurainyun@gmail.com>
2022-10-31 02:04:56 -0700

    Revert "show correct answer"
    This reverts commit a924f596b535d44baea99bd92185946d42aca14d.

--- prototype/Assets/Scripts/AnswerScript.cs
+++ prototype/Assets/Scripts/AnswerScript.cs
@@ -3,22 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-using System.Threading;
-
-
 public class AnswerScript : MonoBehaviour
 {
     public bool isCorrect = false;
     public SanctumQuiz sanctumQuiz;
 
-    public int correctIdx;
-
     public void Answer()
     {
-        if (isCorrect)
+        if(isCorrect)
         {
             GetComponent<Button>().image.color = Color.green;
-
             isCorrect = false;
             //Debug.Log("Correct Answer");
             sanctumQuiz.correct();
@@ -26,14 +20,8 @@ public class AnswerScript : MonoBehaviour
         else
         {
             GetComponent<Button>().image.color = Color.red;
-            if (GameTracker.coins < 10)
-            {
-                sanctumQuiz.options[correctIdx].GetComponent<Button>().image.color = Color.green;
-            }
             //Debug.Log("Incorrect Answer");
             sanctumQuiz.wrong();
-
-
         }
     }
 
