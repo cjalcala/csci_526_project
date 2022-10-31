a924f596b535d44baea99bd92185946d42aca14d
Jiangyun Wang <jurainyun@gmail.com>
2022-10-31 02:02:06 -0700

    show correct answer

--- prototype/Assets/Scripts/AnswerScript.cs
+++ prototype/Assets/Scripts/AnswerScript.cs
@@ -3,16 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+using System.Threading;
+
+
 public class AnswerScript : MonoBehaviour
 {
     public bool isCorrect = false;
     public SanctumQuiz sanctumQuiz;
 
+    public int correctIdx;
+
     public void Answer()
     {
-        if(isCorrect)
+        if (isCorrect)
         {
             GetComponent<Button>().image.color = Color.green;
+
             isCorrect = false;
             //Debug.Log("Correct Answer");
             sanctumQuiz.correct();
@@ -20,8 +26,14 @@ public class AnswerScript : MonoBehaviour
         else
         {
             GetComponent<Button>().image.color = Color.red;
+            if (GameTracker.coins < 10)
+            {
+                sanctumQuiz.options[correctIdx].GetComponent<Button>().image.color = Color.green;
+            }
             //Debug.Log("Incorrect Answer");
             sanctumQuiz.wrong();
+
+
         }
     }
 
