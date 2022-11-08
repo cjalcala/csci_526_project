f985b99e2db30c2d306a4c57f159528e49fc7874
Devang Sharma <sharmadevang11@gmail.com>
2022-11-08 11:16:41 -0800

    Fix Compilation Errors

--- prototype/Assets/Scripts/Coin.cs
+++ prototype/Assets/Scripts/Coin.cs
@@ -26,7 +26,7 @@ public class Coin : MonoBehaviour
 
         if (gameObject.GetComponent<MeshRenderer>().material.color == new Color32(236, 195, 48, 255))
         {
-            GameManager.inst.IncrementCoinCount();
+            //GameManager.inst.IncrementCoinCount();
         }
         //else if (gameObject.GetComponent<MeshRenderer>().material.color == Color.green) {
         //    GameManager.inst.IncrementGreenScore();
