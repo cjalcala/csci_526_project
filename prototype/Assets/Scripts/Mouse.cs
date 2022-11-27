27291ef52e323017863c15cf5e0ca53efe86b15a
Devang Sharma <sharmadevang11@gmail.com>
2022-11-27 01:25:12 -0800

    Add Hammer Immunity to Mouse

--- prototype/Assets/Scripts/Mouse.cs
+++ prototype/Assets/Scripts/Mouse.cs
@@ -29,14 +29,11 @@ public class Mouse : MonoBehaviour
             return;
         }
 
-
-
-        int ingredient_one_count = GameTracker.ingred1;
-        int ingredient_two_count = GameTracker.ingred2;
-        int ingredient_three_count = GameTracker.ingred3;
-
-        if (ingredient_one_count == 0 && ingredient_two_count == 0 && ingredient_three_count == 0)
+        if (other.gameObject.name == "Player" && !Welcome.immunity && !hit && !GameTracker.sanctumImmunity)
         {
+            int ingredient_one_count = GameTracker.ingred1;
+            int ingredient_two_count = GameTracker.ingred2;
+            int ingredient_three_count = GameTracker.ingred3;
             hit = true;
             if (GameTracker.health != 1)
             {
@@ -46,69 +43,89 @@ public class Mouse : MonoBehaviour
             {
                 playerMovement.Die("obstacle");
             }
-        }
-        else if (ingredient_one_count != 0 && ingredient_two_count == 0 && ingredient_three_count == 0)
-        {
-            GameTracker.ingred1 -= 1;
-        }
-        else if (ingredient_one_count == 0 && ingredient_two_count != 0 && ingredient_three_count == 0)
-        {
-            GameTracker.ingred2 -= 1;
-        }
-        else if (ingredient_one_count == 0 && ingredient_two_count == 0 && ingredient_three_count != 0)
-        {
-            GameTracker.ingred3 -= 1;
-        }
-        else if (ingredient_one_count != 0 && ingredient_two_count != 0 && ingredient_three_count == 0)
-        {
-            int rand = Random.Range(1, 3);
-            if (rand == 1)
-            {
-                GameTracker.ingred1 -= 1;
-            }
-            else
-            {
-                GameTracker.ingred2 -= 1;
-            }
-        }
-        else if (ingredient_one_count != 0 && ingredient_two_count == 0 && ingredient_three_count != 0)
-        {
-            int rand = Random.Range(1, 3);
-            if (rand == 1)
-            {
-                GameTracker.ingred1 -= 1;
-            }
-            else
-            {
-                GameTracker.ingred3 -= 1;
-            }
-        }
-        else if (ingredient_one_count == 0 && ingredient_two_count != 0 && ingredient_three_count != 0)
-        {
-            int rand = Random.Range(1, 3);
-            if (rand == 1)
-            {
-                GameTracker.ingred2 -= 1;
-            }
-            else
-            {
-                GameTracker.ingred3 -= 1;
-            }
+
+            // if (ingredient_one_count == 0 && ingredient_two_count == 0 && ingredient_three_count == 0)
+            // {
+                
+            //     if (GameTracker.health != 1)
+            //     {
+            //         GameTracker.health--;
+            //     }
+            //     else
+            //     {
+            //         playerMovement.Die("obstacle");
+            //     }
+            // }
+            // else if (ingredient_one_count != 0 && ingredient_two_count == 0 && ingredient_three_count == 0)
+            // {
+            //     GameTracker.ingred1 -= 1;
+            // }
+            // else if (ingredient_one_count == 0 && ingredient_two_count != 0 && ingredient_three_count == 0)
+            // {
+            //     GameTracker.ingred2 -= 1;
+            // }
+            // else if (ingredient_one_count == 0 && ingredient_two_count == 0 && ingredient_three_count != 0)
+            // {
+            //     GameTracker.ingred3 -= 1;
+            // }
+            // else if (ingredient_one_count != 0 && ingredient_two_count != 0 && ingredient_three_count == 0)
+            // {
+            //     int rand = Random.Range(1, 3);
+            //     if (rand == 1)
+            //     {
+            //         GameTracker.ingred1 -= 1;
+            //     }
+            //     else
+            //     {
+            //         GameTracker.ingred2 -= 1;
+            //     }
+            // }
+            // else if (ingredient_one_count != 0 && ingredient_two_count == 0 && ingredient_three_count != 0)
+            // {
+            //     int rand = Random.Range(1, 3);
+            //     if (rand == 1)
+            //     {
+            //         GameTracker.ingred1 -= 1;
+            //     }
+            //     else
+            //     {
+            //         GameTracker.ingred3 -= 1;
+            //     }
+            // }
+            // else if (ingredient_one_count == 0 && ingredient_two_count != 0 && ingredient_three_count != 0)
+            // {
+            //     int rand = Random.Range(1, 3);
+            //     if (rand == 1)
+            //     {
+            //         GameTracker.ingred2 -= 1;
+            //     }
+            //     else
+            //     {
+            //         GameTracker.ingred3 -= 1;
+            //     }
+            // }
+            // else
+            // {
+            //     int rand = Random.Range(1, 4);
+            //     if (rand == 1)
+            //     {
+            //         GameTracker.ingred1 -= 1;
+            //     }
+            //     else if (rand == 2)
+            //     {
+            //         GameTracker.ingred2 -= 1;
+            //     }
+            //     else
+            //     {
+            //         GameTracker.ingred3 -= 1;
+            //     }
+            // }
         }
         else
         {
-            int rand = Random.Range(1, 4);
-            if (rand == 1)
+            if (!hit)
             {
-                GameTracker.ingred1 -= 1;
-            }
-            else if (rand == 2)
-            {
-                GameTracker.ingred2 -= 1;
-            }
-            else
-            {
-                GameTracker.ingred3 -= 1;
+                Destroy(gameObject);
             }
         }
 
@@ -125,7 +142,7 @@ public class Mouse : MonoBehaviour
     private void OnCollisionExit(Collision collision)
     {
         // if (collision.gameObject.name == "Player" && !Welcome.immunity && hit)
-        if (collision.gameObject.name == "Player" && hit)
+        if (collision.gameObject.name == "Player" && hit && !Welcome.immunity)
         {
             hit = false;
         }
