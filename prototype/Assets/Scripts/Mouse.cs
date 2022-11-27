628c0efc0869237d163268f63e57ac712c445699
tommyshang <56244127+tommyshang@users.noreply.github.com>
2022-11-27 01:47:03 -0800

    Implement mouse lose ingredient or health

--- prototype/Assets/Scripts/Mouse.cs
+++ prototype/Assets/Scripts/Mouse.cs
@@ -27,88 +27,18 @@ public class Mouse : MonoBehaviour
             return;
         }
 
-
-
-        int ingredient_one_count = GameTracker.ingred1;
-        int ingredient_two_count = GameTracker.ingred2;
-        int ingredient_three_count = GameTracker.ingred3;
-
-        if (ingredient_one_count == 0 && ingredient_two_count == 0 && ingredient_three_count == 0)
-        {
+        if (InventorySystemManager.inst.qSize() != 0) {
+            InventorySystemManager.inst.loseRecentIngredient();
+        }
+        else if (GameTracker.health != 1) {
             hit = true;
-            if (GameTracker.health != 1)
-            {
-                GameTracker.health--;
-            }
-            else
-            {
-                playerMovement.Die("obstacle");
-            }
+            GameTracker.health--;
         }
-        else if (ingredient_one_count != 0 && ingredient_two_count == 0 && ingredient_three_count == 0)
-        {
-            GameTracker.ingred1 -= 1;
+        else {
+            playerMovement.Die("obstacle");
         }
-        else if (ingredient_one_count == 0 && ingredient_two_count != 0 && ingredient_three_count == 0)
-        {
-            GameTracker.ingred2 -= 1;
-        }
-        else if (ingredient_one_count == 0 && ingredient_two_count == 0 && ingredient_three_count != 0)
-        {
-            GameTracker.ingred3 -= 1;
-        }
-        else if (ingredient_one_count != 0 && ingredient_two_count != 0 && ingredient_three_count == 0)
-        {
-            int rand = Random.Range(1, 3);
-            if (rand == 1)
-            {
-                GameTracker.ingred1 -= 1;
-            }
-            else
-            {
-                GameTracker.ingred2 -= 1;
-            }
-        }
-        else if (ingredient_one_count != 0 && ingredient_two_count == 0 && ingredient_three_count != 0)
-        {
-            int rand = Random.Range(1, 3);
-            if (rand == 1)
-            {
-                GameTracker.ingred1 -= 1;
-            }
-            else
-            {
-                GameTracker.ingred3 -= 1;
-            }
-        }
-        else if (ingredient_one_count == 0 && ingredient_two_count != 0 && ingredient_three_count != 0)
-        {
-            int rand = Random.Range(1, 3);
-            if (rand == 1)
-            {
-                GameTracker.ingred2 -= 1;
-            }
-            else
-            {
-                GameTracker.ingred3 -= 1;
-            }
-        }
-        else
-        {
-            int rand = Random.Range(1, 4);
-            if (rand == 1)
-            {
-                GameTracker.ingred1 -= 1;
-            }
-            else if (rand == 2)
-            {
-                GameTracker.ingred2 -= 1;
-            }
-            else
-            {
-                GameTracker.ingred3 -= 1;
-            }
-        }
+       
+
 
         // hit = false;
         //Destroy(gameObject);
